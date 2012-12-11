032f2daddff5947bf27c10bb8db2348c3951d1d3
Jose Peleteiro <jose@peleteiro.net>
2012-12-11 12:13:49 -0200

    Configuração melhorada do jedi no emacs

--- home/.emacs.d/modules/python-jedi.el
+++ home/.emacs.d/modules/python-jedi.el
@@ -1,3 +1,7 @@
 
-(add-to-list 'el-get-sources '(:name jedi))
+(add-to-list 'el-get-sources '(:name jedi
+                               :post-init (progn
+                                            (setq jedi:setup-keys t)
+                                            (add-hook 'python-mode-hook 'jedi:setup)
+                              )))
 
