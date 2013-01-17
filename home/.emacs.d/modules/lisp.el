7362dd713fc00efd068e0860e1607998fef56fb8
Jose Peleteiro <jose@peleteiro.net>
2013-01-17 11:52:11 -0200

    [emacs] configuração para lisp

---
+++ home/.emacs.d/modules/lisp.el
@@ -0,0 +1,10 @@
+(setq lisp-indent-offset 2)
+
+(add-to-list 'el-get-sources
+             '(:name slime))
+
+(add-to-list 'el-get-sources
+             '(:name ac-slime))
+
+(add-to-list 'el-get-sources
+             '(:name elisp-slime-nav))
