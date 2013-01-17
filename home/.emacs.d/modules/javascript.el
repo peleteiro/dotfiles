988dbe069f7063988e40c5123f83a9ae43e5370d
Jose Peleteiro <jose@peleteiro.net>
2013-01-17 11:37:04 -0200

    [emacs] javascript melhorado e com auto-complete

--- home/.emacs.d/modules/javascript.el
+++ home/.emacs.d/modules/javascript.el
@@ -1,19 +1,17 @@
 (add-to-list 'el-get-sources
-  '(:name js2-mode
-    :type git
-    :url "https://github.com/mooz/js2-mode.git"
-    :prepare (autoload 'js2-mode "js2-mode" nil t)
-    :post-init (progn
-      (autoload 'js2-mode "js2-mode" nil t)
-      (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+             '(:name js3-mode
+                     :post-init (progn
+                                  (autoload 'js3-mode "js3-mode" nil t)
+                                  (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
 
-      (setq js-indent-level 2)
-      (setq js2-basic-offset 2)
-      (setq js2-cleanup-whitespace t)
+                                  (setq js-indent-level 2)
+                                  (setq js3-basic-offset 2)
+                                  (setq js3-cleanup-whitespace t)
 
-    )))
+                                  (add-hook 'js3-mode-hook '(lambda ()
+                                                               (coding-hook)
+                                                               (auto-complete-mode))))))
 
 
 (add-to-list 'el-get-sources
-  '(:name coffee-mode))
-
+             '(:name coffee-mode))
