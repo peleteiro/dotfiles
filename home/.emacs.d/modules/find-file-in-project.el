5b47cae4ff70ee6128c7a134450df6a251ef3588
Jose Peleteiro <jose@peleteiro.net>
2013-02-01 00:52:29 -0200

    [emacs] coffee e handlebars no find-in-project

--- home/.emacs.d/modules/find-file-in-project.el
+++ home/.emacs.d/modules/find-file-in-project.el
@@ -4,6 +4,7 @@
       (require 'find-file-in-project)
       (setq ffip-patterns
             '("*.json" "*.html" "*.org" "*.txt" "*.md" "*.el" "*.clj" "*.py" "*.rb" "*.js" "*.less" "Procfile"
+              "*.hbs" "*.handlebars" "*.coffee"
               "*.pl" "*.sh" "*.erl" "*.hs" "*.ml" "*.css" "*.scss" "*.sass" "*.haml" "*.slim" "Gemfile" "*.erb"))
 
       (setq ffip-find-options "-not -regex \".*/node_modules/.*\" -not -regex \".*/vendor/.*\" -not -regex \".*/public/system/.*\"")
