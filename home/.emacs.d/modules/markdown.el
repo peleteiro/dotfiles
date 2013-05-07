026889cd9842384d2df9193577868c49f8ed3d51
Jose Peleteiro <jose@peleteiro.net>
2013-05-06 21:06:03 -0300

    [emacs] scala added

--- home/.emacs.d/modules/markdown.el
+++ home/.emacs.d/modules/markdown.el
@@ -5,5 +5,6 @@
     :load "markdown-mode.el"
     :post-init (progn
              (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+             (setq markdown-command "/usr/local/bin/markdown")
              (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
              (setq auto-mode-alist (cons '("\\.text" . markdown-mode) auto-mode-alist)))))
---
+++ home/.emacs.d/modules/scala.el
@@ -0,0 +1,1 @@
+(add-to-list 'el-get-sources '(:name scala-mode2))
